179a9c4078aae04708dbf7ae9ab3f0df5b671957
LowM <513499003@qq.com>
2018-11-26 11:11:06 +0800

    Initial Commit

---
+++ MyC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyC
+//
+//  Created by 吴浩 on 2018/11/26.
+//  Copyright © 2018 吴浩. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
